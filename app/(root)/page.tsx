c2e6847fb229e8a28ae3c17e7584c44a843bfe51
Frank Wang <frankningwang@gmail.com>
2025-07-25 15:41:25 -0400

    implement layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 function Home() {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Frank",
+    lastName: "Wang",
+    email: "example@gmail.com",
+  };
+
+  return (
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 456.75 }]}
+      />
+    </div>
+  );
 }
 
 export default Home;
